f4dc5572b84c2fdf4a7a8527f9f8e0df16c09ec1
manojbabup <manojbabu.partharaj@cognizant.com>
2020-05-17 20:32:28 -0400

    Second Assign

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,31 @@
 package io.pivotal.pal.tracker;
 
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @RestController
 public class WelcomeController {
 
+    @Value("${welcome.message}")
+    String welcomeMessage;
+
+//    String Welcome1 = welcomeMessage;
+//
+//    public WelcomeController(String welcome) {
+//        this.welcomeMessage = welcome;
+//    }
+//
+//    public WelcomeController() {
+//        System.out.println("Wlcome");
+//    }
+
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return welcomeMessage;
     }
 }
